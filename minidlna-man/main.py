7b73697285b67c3ab43670dbb14a07e218dd6b09
Zsolt Alfoldi <alfonzso@gmail.com>
2021-06-21 01:21:13 +0100

    .

--- minidlna-man/main.py
+++ minidlna-man/main.py
@@ -2,8 +2,6 @@ from docker.models.containers import Container
 import docker
 from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
 
-# import jinja2
-
 import json
 import logging
 import sys
@@ -20,6 +18,18 @@ host_name = os.getenv("MINIDLNA_MAN_HOST", "0.0.0.0")
 host_port = os.getenv("MINIDLNA_MAN_PORT", 1032)
 minidlna_server_container_name_pattern = os.getenv("MINIDLNA_SERVER_CONTAINER_NAME_PATTERN", "minidlna-server")
 
+# volumes
+minidlna_volume_enabled = bool(os.getenv("MINIDLNA_VOLUME_ENABLED", False))
+
+
+if minidlna_volume_enabled:
+    minidlna_volume_name = os.getenv("MINIDLNA_VOLUME_NAME", "transmission-data")
+    minidlna_volume_type = os.getenv("MINIDLNA_VOLUME_TYPE", "nfs")
+    minidlna_volume_addr = os.getenv("MINIDLNA_VOLUME_ADDR")
+    if minidlna_volume_addr is None:
+        sys.exit("MINIDLNA_VOLUME_ADDR missing ! Terminating ... ")
+    minidlna_volume_opts = os.getenv("MINIDLNA_VOLUME_OPTS", f'nfsvers=4,addr={minidlna_volume_addr},nfsvers=4,nolock,soft')
+    minidlna_volume_device = os.getenv("MINIDLNA_VOLUME_OPTS", ':/mnt/hdd/transmission-workspace/downloads/complete/')
 
 def get_content_len(path):
     return str(os.path.getsize(path))
@@ -31,12 +41,6 @@ class MyServer(BaseHTTPRequestHandler):
         "_404_html": None,
         "_javascript": None
     }
-    # _path_of_index_html = None
-    # # _javascript_as_string = None
-    # _404_html = None
-    # _javascript = None
-    # _url = {"URL": "http://pi.local.net:1032/restart"}
-    # _url = {"URL": "/restart"}
 
     class Employee(object):
         def __init__(self, _dict):
@@ -45,30 +49,72 @@ class MyServer(BaseHTTPRequestHandler):
     def init(self):
         root.info("__init__")
         cwd = os.path.dirname(os.path.realpath(__file__))
-        # self._path_of.get('')
         self._path_of = self.Employee(self._paths)
 
         self._path_of._index_html = f"{cwd}/res/html/index.html"
         self._path_of._404_html = f"{cwd}/res/html/404.html"
         self._path_of._javascript = f"{cwd}/res/js/main.js"
 
-        # templateLoader = jinja2.FileSystemLoader(searchpath="/")
-        # templateEnv = jinja2.Environment(loader=templateLoader)
-        # template = templateEnv.get_template(self._javascript)
-        # self._javascript_as_string = template.render(self._url)
-
     @staticmethod
     def docker():
         client = docker.from_env()
-        docker_ps = client.containers.list()
-        docker_cli_minidlna: Container = [cont for cont in docker_ps if minidlna_server_container_name_pattern in cont.name][0]
-        docker_cli_minidlna.exec_run("/bin/bash -c \" minidlna -R \" ")
-        res = docker_cli_minidlna.exec_run("/bin/bash -c \" /usr/sbin/minidlnad -R \" ")
-        docker_cli_minidlna.restart()
-        # import time
-        # time.sleep(2)
-        # res = ""
-        return f"{res} - {docker_cli_minidlna.status}"
+
+        containers = [cont for cont in client.containers.list() if minidlna_server_container_name_pattern in cont.name]
+        dcli_minidlna: Container = containers[0] if len(containers) > 0 else None
+
+        if dcli_minidlna:
+            dcli_stop = dcli_minidlna.remove(v=True, force=True)
+        else:
+            if minidlna_volume_enabled:
+                vols = [vol for vol in client.volumes.list() if minidlna_volume_name in vol.name]
+                if len(vols) > 0:
+                    client.volumes.get(minidlna_volume_name).remove(force=True)
+
+        vols_dict = {}
+        if minidlna_volume_enabled:
+            create_vol = client.volumes.create(
+                name=minidlna_volume_name, driver='local',
+                # driver_opts={
+                #     'type': 'nfs',
+                #     'o': 'nfsvers=4,addr=192.168.1.121,nfsvers=4,nolock,soft',
+                #     'device': ':/mnt/hdd/transmission-workspace/downloads/complete/'
+                # },
+                driver_opts={
+                    'type': minidlna_volume_type,
+                    'o': minidlna_volume_opts,
+                    'device': minidlna_volume_device
+                }
+            )
+
+            vols_dict = {
+                "volumes": {
+                    minidlna_volume_name: {'bind': '/media/video', 'mode': 'rw'}
+                }
+            }
+
+        # tst = vols_dict if minidlna_volume_enabled else {}
+        # tst = vols_dict
+        minidlna_srvr = client.containers.run(
+            image="vladgh/minidlna",
+            detach=True,
+            name=minidlna_server_container_name_pattern,
+            user="root",
+            restart_policy={"Name": "always"},
+            network_mode="host",
+            **vols_dict,
+            environment=[
+                "TINI_SUBREAPER=register",
+                "MINIDLNA_MEDIA_DIR_1=/media/video",
+                "MINIDLNA_FRIENDLY_NAME=RPI_MINIDLNA",
+                "MINIDLNA_INOTIFY=yes",
+                "MINIDLNA_NOTIFY_INTERVAL=120",
+                "MINIDLNA_PORT=8200",
+                "MINIDLNA_LOG_LEVEL=general,artwork,database,inotify,scanner,metadata,http,ssdp,tivo=info",
+                "MINIDLNA_WIDE_LINKS=yes",
+            ],
+        )
+
+        return f"srvr stat - {minidlna_srvr.status}"
 
     def do_GET(self):
         self.init()
